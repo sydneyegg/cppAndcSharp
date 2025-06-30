79c612b3adf299b6ae2dcb174adb4485060b10f9
sydneyegg <sydney.eggleston@sonarsource.com>
2025-06-30 14:14:13 -0500

    Update CPlusPlusAndCSharp.cpp

--- CPlusPlusAndCSharp/CPlusPlusAndCSharp.cpp
+++ CPlusPlusAndCSharp/CPlusPlusAndCSharp.cpp
@@ -29,5 +29,4 @@ void f()
 //   3. Use the Output window to see build output and other messages
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-//   7. something 
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file 
